39275b2e17b1328a4567e64a3ffce16791d12de0
Jatin <digrajatin@gmail.com>
2022-12-31 20:20:24 +0530

    first commit

---
+++ 216. Combination Sum III.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void solve(int k , int n , vector<int>temp , vector<vector<int>> &ans , int sum , int curr)
+    {
+    	// if no of digits pick == k and their sum = required sum then !!
+        if(temp.size()==k && sum==n)
+        {
+            ans.push_back(temp);
+	            
+	            // printing op // for debug only // not needed on LC
+	            for(auto &i:temp)
+	            	cout<<i<<" ";
+	            cout<<endl;
+            return;
+        }
+
+        // if sum exceeds n no meaning of picking new nos
+        // or if no of digits picked > k
+        // or if no digit left to pick !!
+        if(sum>n || temp.size()>k || curr>9)
+            return;
+        
+        // not pick the curr number !!
+        solve(k,n,temp,ans,sum,curr+1);
+        // pick the curr number !!
+        temp.push_back(curr);
+        solve(k,n,temp,ans,sum+curr,curr+1);
+    }
+
+int main()
+{
+	int k , n;
+	cin>>k;
+	cin>>n;
+	vector<vector<int>> ans;
+    vector<int>temp;
+    // order of picking nos does not matter as sum will stay same !!
+    // can only pick a number once so only 2 cases pick or not pick !!
+    solve(k,n,temp,ans,0,1);
+
+    // return the ans vector in LC 
+
+	return 0;
+}
